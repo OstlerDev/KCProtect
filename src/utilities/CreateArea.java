281ed093390e3e1c1752a5e04bd938c6cc1a456b
OstlerDev <ostlerdev@me.com>
2012-06-27 08:25:26 -0600

    Added CreateArea util
    Added CreateArea(Vector xz1, Vector xz2) it will generate a yml in the
    areas folder later on.

---
+++ src/utilities/CreateArea.java
@@ -0,0 +1,27 @@
+package utilities;
+
+import org.bukkit.util.Vector;
+/**
+ * 
+ * @author OstlerDev
+ *
+ */
+public class CreateArea {
+
+	public CreateArea(Vector xz1, Vector xz2) {
+		/**
+		 * 
+		 *  TODO: Generate a yml file for the region I suggest it looks something like, but we can make it whatever
+		 *  
+		 *  Owner: OstlerDev
+		 *  Members:
+		 *  Vector1x: 123
+		 *  Vector1z: 104
+		 *  Vector2x: 205
+		 *  Vector2z: 125
+		 *  Flags: nomobs, nopvp
+		 *  
+		 */
+	}
+
+}
