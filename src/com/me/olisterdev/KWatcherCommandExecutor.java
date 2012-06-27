a3f670ec5361a6d55d122e6b834c9bddfd64ad5d
Desmond Jackson <desmondjackson1@gmail.com>
2012-06-27 04:47:16 -0500

    creating the command executor.

---
+++ src/com/me/olisterdev/KWatcherCommandExecutor.java
@@ -0,0 +1,16 @@
+package com.me.olisterdev;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class KWatcherCommandExecutor implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,
+			String[] arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
