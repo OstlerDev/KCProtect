608a8a6897460c777fa8f06e7d5d19eb418c0b9a
Desmond Jackson <desmondjackson1@gmail.com>
2012-06-27 19:54:04 -0500

    renamed to KCProtect
    (Named it incorrectly)
    Excuse: 4:00 A.M.

---
+++ src/com/me/ostlerdev/KCProtect.java
@@ -0,0 +1,46 @@
+package com.me.ostlerdev;
+
+import java.util.logging.Logger;
+
+import listeners.BlockBreakListener;
+import listeners.BlockPlaceListener;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import utilities.Config;
+import utilities.CreateUtilities;
+
+/**
+ * @author DarkGhoul45
+ */
+public class KCProtect extends JavaPlugin {
+
+	// Logger is needed to display information in the future. Don't remove it.
+	private Logger logger;
+
+	public KCProtect() {
+		super();
+	}
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		logger = getLogger();
+		logger.info("KWatcher has been enabled!");
+		new CreateUtilities();
+		new Config(this);
+		KCProtectCommandExecutor executor = new KCProtectCommandExecutor();
+		// TODO add the rest of the commands.
+		getCommand("kc").setExecutor(executor);
+		// TODO add the rest of the listeners. (if any)
+		getServer().getPluginManager().registerEvents(new BlockBreakListener(this), this);
+		getServer().getPluginManager().registerEvents(new BlockPlaceListener(), this);
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+		logger.info("KWatcher has been disabled!");
+	}
+
+}
