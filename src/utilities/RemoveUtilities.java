f549ec975ba303929c731f79ba03ca0b5262679f
Desmond Jackson <desmondjackson1@gmail.com>
2012-06-27 19:54:24 -0500

    New remove Utilities class

---
+++ src/utilities/RemoveUtilities.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import java.io.File;
+
+/**
+ * @author DarkGhoul45
+ */
+public class RemoveUtilities extends Thread {
+
+	private String playerName;
+
+	public RemoveUtilities(String playerName) {
+		super();
+		this.playerName = playerName;
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		File file = new File("plugins/KWatcher/Areas/" + playerName + "Area.txt");
+		if(file.exists()) {
+			file.delete();
+		}
+	}
+
+}
