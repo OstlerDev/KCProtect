d2a507bab3e337ef56b23186188a125de2b8b628
Desmond Jackson <desmondjackson1@gmail.com>
2012-06-28 15:16:58 -0500

    Update, test code.

--- src/com/me/ostlerdev/KCProtectCommandExecutor.java
+++ src/com/me/ostlerdev/KCProtectCommandExecutor.java
@@ -3,20 +3,16 @@ package com.me.ostlerdev;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 /**
- * @author OlisterDev
  * @author DarkGhoul45
  */
 public class KCProtectCommandExecutor implements CommandExecutor {
 
+	// TODO Implement this
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if(command.getName().equals("kc")) {
-			// TODO Handle all of the commands.
-			@SuppressWarnings("unused")
-			Player player = (Player) sender;
 		}
 		return false;
 	}
