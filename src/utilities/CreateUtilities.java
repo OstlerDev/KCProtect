6ecf947d937e8413c4f655fdfa032d2f642d5a5f
Desmond Jackson <desmondjackson1@gmail.com>
2012-06-27 19:53:38 -0500

    create utilities

--- src/utilities/CreateUtilities.java
+++ src/utilities/CreateUtilities.java
@@ -1,22 +1,59 @@
 package utilities;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 /**
  * @author DarkGhoul45
  */
 public class CreateUtilities extends Thread {
 
+	private Player player;
+	private Location location;
+
 	public CreateUtilities() {
-		super();
 		File[] directories = {
 				new File("plugins/KWatcher/Areas/"),
 				new File("plugins/KWatcher/Logs/")
 		};
-		for(File f : directories) {
-			if(!f.exists()) {
-				f.mkdirs();
+		for(File file : directories) {
+			if(!file.exists()) {
+				file.mkdirs();
 			}
+		}
+	}
+
+	// TODO Call this from the BlockPlaceListener.class
+	public CreateUtilities(Player player, Location location) {
+		super();
+		this.player = player;
+		this.location = location;
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		File file = new File("plugins/KWatcher/Areas/" + player.getName() + "Area.txt");
+		if(!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			FileWriter fstream = new FileWriter(file);
+			BufferedWriter writer = new BufferedWriter(fstream);
+			writer.write(location.getBlockX() + "," + location.getBlockY() + "," + location.getBlockZ());
+			writer.close();
+			fstream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
