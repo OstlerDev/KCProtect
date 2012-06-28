a87bdede2f8ea1513f39c90cf7e6cee36d9f90d2
Desmond Jackson <desmondjackson1@gmail.com>
2012-06-27 19:53:21 -0500

    config file

--- src/utilities/Config.java
+++ src/utilities/Config.java
@@ -2,7 +2,7 @@ package utilities;
 
 import org.bukkit.plugin.Plugin;
 
-import com.me.ostlerdev.KWatcher;
+import com.me.ostlerdev.KCProtect;
 
 public class Config {
 
@@ -13,18 +13,18 @@ public class Config {
 	public Config(Plugin plugin){
 		p = plugin;
 	}
-	
-	public Config(KWatcher plugin) {
+
+	public Config(KCProtect plugin) {
 		LoadConfig();
 	}
 
 	public boolean LoadConfig(){
 		/**
-		 * 
+		 *
 		 * TODO:
 		 * Decide what plot sizes we are going to use and what blocks default
 		 * then add an option to the config
-		 * 
+		 *
 		 */
 		p.getConfig().addDefault("Utilities.VerboseMode", true);
 		p.getConfig().options().copyDefaults(true);
